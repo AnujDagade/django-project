0ba9e3d80b6102e70f3cb9d09894903c3138d4d1
Anuj Dagade <dagadeanuj@gmail.com>
2024-04-25 08:58:03 +0000

    Wrong answers display added

--- Quiz_App_Project_using_Django_Framework/Quiz_App_Project_using_Django_Framework/static/js/quiz.js
+++ Quiz_App_Project_using_Django_Framework/Quiz_App_Project_using_Django_Framework/static/js/quiz.js
@@ -4,39 +4,27 @@ function getName(){
 	alert("Hi, " + login);
 	return login;
 }
+
+
+
+
 function submitAnswers(event, answers){
 	event.preventDefault();
 
-	
+
 	var total = answers.length;
 	var score = 0;
 	var choice = []
 	wrongAnswers = []
 
-	//getting choices 
-	/*
-	var q1 = document.forms["quizForm"]["q1"].value;
-	var q2 = document.forms["quizForm"]["q2"].value;
-	var q3 = document.forms["quizForm"]["q3"].value;
-	var q4 = document.forms["quizForm"]["q4"].value;
-	var q5 = document.forms["quizForm"]["q5"].value;
-*/
-//new dynamic method 1
+	
 	for(var i = 1; i <= total; i++){
 		
 		choice[i] = document.forms["quizForm"]["q"+i].value;
 	}
  
 
-	//validation
-	/*
-	for(i = 1; i <= total; i++){
-		if(eval('q'+i) == null || eval('q'+i) == ''){
-			alert('you missed question ' + i);
-			return false;
-		}
-	}
-	*/
+
 	for(i = 1; i <= total; i++){
 		if(choice[i] == null || choice[i] == ''){
 			alert('you missed question ' + i);
@@ -45,19 +33,7 @@ function submitAnswers(event, answers){
 	}
 
 
-	//set correct answer
-	//var answers = ["b", "d", "a", "c", "c"];
-	//this variable  is replaced by database answer
-
-	//check answer
-	/*
-	for(i = 1; i <= total; i++){
-		if(eval('q' + i) == answers[i - 1]){
-			score++;
-		}
-	}
-	*/
-	// new dynamic method 1 for checking answer
+	
 	for(i = 1; i <= total; i++){
 		if(choice[i] == answers[i - 1]){
 			score++;
@@ -69,8 +45,17 @@ function submitAnswers(event, answers){
 	//Display Result
 	var results = document.getElementById('results');
 	results.innerHTML = "<h3>You scored <span>" + score + "</span> out of <span>" + total + "</span></h3>"
-	alert("You scored " + score + " out of " + total);
-
 	
+	let wrongAnsEle = document.getElementById('wrong-answers');
+	wrongAnsEle.innerHTML = `<h3>Wrong Answers: </h3>
+	<ul>
+	${wrongAnswers.map((ans) => `<li>Question ${ans}</li>`).join('')}
+	</ul>`;
+
+	setTimeout(()=>{
+		window.scrollTo(0, 0);
+	})
+
+	console.log(wrongAnswers);
 }
 
